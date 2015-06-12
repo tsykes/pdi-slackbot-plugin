f61198b4d7ab97e1180af78807e212a1378ec24b
Andrew FTB <aoverton@findthebest.com>
2015-06-12 15:29:18 -0400

    slackbot: add meta information for storing and reading xml

--- src/com/findthebest/slack/SlackBot.java
+++ src/com/findthebest/slack/SlackBot.java
@@ -23,9 +23,12 @@
 package com.findthebest.slack;
 
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.pentaho.di.cluster.SlaveServer;
+import org.pentaho.di.core.Const;
 import org.pentaho.di.core.Result;
 import org.pentaho.di.core.annotations.JobEntry;
 import org.pentaho.di.core.database.DatabaseMeta;
@@ -40,8 +43,6 @@ import org.pentaho.di.repository.ObjectId;
 import org.pentaho.di.repository.Repository;
 import org.pentaho.metastore.api.IMetaStore;
 import org.w3c.dom.Node;
-
-import com.findthebest.slack.SlackConnection;
 
 /**
  * This defines a 'create folder' job entry. Its main use would be to create empty folder that can be used to control
@@ -70,7 +71,9 @@ public class SlackBot extends JobEntryBase implements Cloneable, JobEntryInterfa
 
     // This field holds the configured result of the job entry.
     // It is configured in the SlackBotDialog
-    private boolean outcome;
+    private String selectedChannel, customText;
+    private boolean successMsg, failureMsg, customMsg;
+    private LinkedList<String> channelList;
 
     /**
      * The JobEntry constructor executes super() and initializes its fields
@@ -82,7 +85,12 @@ public class SlackBot extends JobEntryBase implements Cloneable, JobEntryInterfa
         super(name, "");
 
         // the default is to generate a positive outcome
-        outcome = true;
+        selectedChannel = "";
+        successMsg = true;
+        failureMsg = false;
+        customMsg = false;
+        customText = "";
+        channelList = new LinkedList<String>();
     }
     /**
      * No-Arguments constructor for convenience purposes.
@@ -110,8 +118,13 @@ public class SlackBot extends JobEntryBase implements Cloneable, JobEntryInterfa
      * @return a deep copy of this
      */
     public Object clone(){
-        SlackBot je = (SlackBot) super.clone();
-        return je;
+        SlackBot retval = (SlackBot) super.clone();
+        retval.channelList = new LinkedList<String>();
+        ListIterator<String> listIterator = channelList.listIterator();
+        for (String channel : channelList) {
+            retval.channelList.add(channel);
+        }
+        return retval;
     }
 
     /**
@@ -125,11 +138,18 @@ public class SlackBot extends JobEntryBase implements Cloneable, JobEntryInterfa
      */
     @Override
     public String getXML(){
-        StringBuffer retval = new StringBuffer(200);
+        StringBuffer retval = new StringBuffer(1000);
 
         retval.append(super.getXML());
-        retval.append("      ").append(XMLHandler.addTagValue("outcome",outcome));
-
+        retval.append("      ").append(XMLHandler.addTagValue("selectedChannel", selectedChannel));
+        retval.append("      ").append(XMLHandler.addTagValue("customText", customMsg));
+        retval.append("      ").append(XMLHandler.addTagValue("successMsg", successMsg));
+        retval.append("      ").append(XMLHandler.addTagValue("failureMsg", failureMsg));
+        retval.append("      <channels>" + Const.CR);
+        for (String channel : channelList) {
+            retval.append("        ").append(XMLHandler.addTagValue("channel", channel));
+        }
+        retval.append("      </channels>" + Const.CR);
         return retval.toString();
     }
 
@@ -152,7 +172,20 @@ public class SlackBot extends JobEntryBase implements Cloneable, JobEntryInterfa
 
         try{
             super.loadXML(entrynode, databases, slaveServers);
-            outcome = "Y".equalsIgnoreCase(XMLHandler.getTagValue(entrynode, "outcome"))?true:false;
+
+            selectedChannel = XMLHandler.getTagValue(entrynode, "selectedChannel");
+            successMsg = "Y".equalsIgnoreCase(XMLHandler.getTagValue(entrynode, "successMsg"));
+            failureMsg = "Y".equalsIgnoreCase(XMLHandler.getTagValue(entrynode, "failureMsg"));
+            customMsg = "Y".equalsIgnoreCase(XMLHandler.getTagValue(entrynode, "customMsg"));
+            customText = XMLHandler.getTagValue(entrynode, "customText");
+            // populate channel list
+            Node channels = XMLHandler.getSubNode( entrynode, "channels" );
+            int count = XMLHandler.countNodes( channels, "channel" );
+            channelList = new LinkedList<String>();
+            for ( int i = 0; i < count; i++ ) {
+                Node fnode = XMLHandler.getSubNodeByNr( channels, "channel", i );
+                channelList.add(fnode.getNodeValue());
+            }
         }
         catch(Exception e){
             throw new KettleXMLException(BaseMessages.getString(PKG, "Demo.Error.UnableToLoadFromXML"), e);
@@ -171,7 +204,7 @@ public class SlackBot extends JobEntryBase implements Cloneable, JobEntryInterfa
     public void saveRep(Repository rep, IMetaStore metaStore, ObjectId id_job) throws KettleException{
 
         try{
-            rep.saveJobEntryAttribute(id_job, getObjectId(), "outcome", outcome);
+            rep.saveJobEntryAttribute(id_job, getObjectId(), "selectedChannel", selectedChannel);
         }
         catch(KettleDatabaseException dbe){
             throw new KettleException(BaseMessages.getString(PKG, "Demo.Error.UnableToSaveToRepository")+id_job, dbe);
@@ -191,7 +224,7 @@ public class SlackBot extends JobEntryBase implements Cloneable, JobEntryInterfa
     @Override
     public void loadRep(Repository rep, IMetaStore metaStore, ObjectId id_jobentry, List<DatabaseMeta> databases, List<SlaveServer> slaveServers) throws KettleException{
         try{
-            outcome = rep.getJobEntryAttributeBoolean(id_jobentry, "outcome");
+            selectedChannel = rep.getJobEntryAttributeString(id_jobentry, "selectedChannel");
         }
         catch(KettleDatabaseException dbe){
             throw new KettleException(BaseMessages.getString(PKG, "Demo.Error.UnableToLoadFromRepository")+id_jobentry, dbe);
@@ -210,10 +243,18 @@ public class SlackBot extends JobEntryBase implements Cloneable, JobEntryInterfa
      */
     public Result execute(Result prev_result, int nr){
         int errors = 0;
-        outcome = true;
+        boolean outcome = true;
         try {
             SlackConnection slack = new SlackConnection();
-            slack.postToSlack("C061NB8LD", "Testing...");
+            String msg;
+            if (successMsg) {
+                msg = "Job executed successfully";
+            } else if (failureMsg) {
+                msg = "Job failed";
+            } else {
+                msg = customText;
+            }
+            slack.postToSlack(selectedChannel, msg);
         } catch (Exception e) {
             logBasic(e.getMessage());
             errors++;
@@ -242,20 +283,52 @@ public class SlackBot extends JobEntryBase implements Cloneable, JobEntryInterfa
         return false;
     }
 
-    /**
-     * Getter.
-     * @return the configured outcome for the job entry
-     */
-    public boolean getOutcome() {
-        return outcome;
+    public String getSelectedChannel() {
+        return selectedChannel;
     }
 
-    /**
-     * Setter.
-     * @param outcome the configured outcome for the job entry
-     */
-    public void setOutcome(boolean outcome) {
-        this.outcome = outcome;
+    public void setSelectedChannel(String selectedChannel) {
+        this.selectedChannel = selectedChannel;
+    }
+
+    public String getCustomText() {
+        return customText;
+    }
+
+    public void setCustomText(String customText) {
+        this.customText = customText;
+    }
+
+    public boolean isSuccessMsg() {
+        return successMsg;
+    }
+
+    public void setSuccessMsg(boolean successMsg) {
+        this.successMsg = successMsg;
+    }
+
+    public boolean isFailureMsg() {
+        return failureMsg;
+    }
+
+    public void setFailureMsg(boolean failureMsg) {
+        this.failureMsg = failureMsg;
+    }
+
+    public boolean isCustomMsg() {
+        return customMsg;
+    }
+
+    public void setCustomMsg(boolean customMsg) {
+        this.customMsg = customMsg;
+    }
+
+    public LinkedList<String> getChannelList() {
+        return channelList;
+    }
+
+    public void setChannelList(LinkedList<String> channelList) {
+        this.channelList = channelList;
     }
 
 }
